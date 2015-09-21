5473c1ee4951add65d692f8ee35cb16dc3d05490
fernandoGuisso <fguisso@gmail.com>
2015-09-21 00:23:03 -0300

    add js documents and update server

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express')
   , app = express()
   , http = require('http').Server(app)
+  , io = require('socket.io')(http);
   ;
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected');
+  socket.on('disconnect', function(){
+    console.log('A user disconnected')
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
